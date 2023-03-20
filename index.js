7febb8111db19ada49fd1237634fcbc927cfbff4
florad16 <106739555+florad16@users.noreply.github.com>
2023-03-20 12:25:21 +0100

    1 file modified

--- index.js
+++ index.js
@@ -51,6 +51,92 @@ let rabbit = new Rabbit ("White Rabbit");
 console.log(rabbit.name);
 
 //Aufgabe8 
+class Node {
+  constructor(value) {
+    if (typeof value !== "number") {
+      throw new Error("Nur Zahlen sind erlaubt.");
+    }
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class List {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  insertLast(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+
+  insertFirst(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+  }
+
+  search(value) {
+    let currentNode = this.head;
+    while (currentNode) {
+      if (currentNode.value === value) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
+  }
+
+  delete(value) {
+    if (!this.head) {
+      return;
+    }
+
+    if (this.head.value === value) {
+      this.head = this.head.next;
+      if (!this.head) {
+        this.tail = null;
+      }
+      return;
+    }
+
+    let currentNode = this.head;
+    while (currentNode.next) {
+      if (currentNode.next.value === value) {
+        currentNode.next = currentNode.next.next;
+        if (!currentNode.next) {
+          this.tail = currentNode;
+        }
+        return;
+      }
+      currentNode = currentNode.next;
+    }
+  }
+
+  print() {
+    let currentNode = this.head;
+    const values = [];
+    while (currentNode) {
+      values.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    console.log(values.join(" -> "));
+  }
+}
+
 
 
 
